17a551ae182d46a4a80756962767e8ad3fb3cf42
MaxX771 <maxxnovikoff@gmail.com>
2021-12-20 18:58:11 +0400

    add._average_s

--- Work_on_students.py
+++ Work_on_students.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname, age):
         self.name = name
         self.surname = surname
         self.gender = []
@@ -10,25 +10,32 @@ class Student:
         self.courses_attached = []
 
     def rate_lectures(self, lecturer, new_course, grade):
-         if isinstance(lecturer,Lecturer):
+        if isinstance(lecturer, Lecturer):
             if new_course in lecturer.grades:
                 lecturer.grades[new_course] += [grade]
             else:
                 lecturer.grades[new_course] = [grade]
-         else:
+        else:
             return 'Ошибка'
 
+    def _average_s(self):
+        rec = []
+        for v in self.grades.values():
+            rec += v
+        average_s = round(sum(rec) / len(rec), 1)
+        return average_s
+
     def __str__(self):
-        for k, v in self.grades.items():
-            student.grades = (round(sum(v) / len(v), 2))
-        return f'\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {student.grades}' \
-                f'\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {self.finished_courses[0]}'
+        return f'\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self._average_s()}' \
+        f'\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {self.finished_courses[0]}'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
 
 class Lecturer(Mentor):
 
@@ -39,15 +46,16 @@ class Lecturer(Mentor):
         self.courses_in_progress = []
         self.grades = {}
 
+    def _average_l(self):
+        res = []
+        for v in self.grades.values():
+            res += v
+        average = round(sum(res) / len(res), 1)
+        return average
 
     def __str__(self):
-        for k, v in self.grades.items():
-            lecturer.grades = (round(sum(v) / len(v), 2))
-        return f'\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {lecturer.grades}' \
-                    f'\nЧитает лекции по курсу: {self.courses_in_progress[0]}'
-
-
-
+        return f'\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._average_l()}' \
+               f'\nЧитает лекции по курсу: {self.courses_in_progress[0]}'
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -62,6 +70,9 @@ class Reviewer(Mentor):
     def __str__(self):
         return f'\nИмя: {self.name}\nФамилия: {self.surname}'
 
+
+
+
 cool_mentor = Mentor('Some', 'Buddy')
 # cool_mentor.courses_attached += ['Python']
 
@@ -73,20 +84,19 @@ student = Student('Rio', 'Eman', 'your_gender')
 student.courses_in_progress += ['Python', 'Git']
 student.courses_attached += ['Python']
 student.finished_courses += ['Введение в программирование']
-some_student = student
-
+some_student = student1
+student1.__lt__(student)
 
 lecturer1 = Lecturer('Roy', 'Jones')
 lecturer1.courses_in_progress += ['Git']
 lecturer = Lecturer('Jone', 'Simse')
 lecturer.courses_in_progress += ['Python']
-some_lecturer = lecturer
+some_lecturer = lecturer1
 
-
-student.rate_lectures(lecturer, 'Git', 10)
-student.rate_lectures(lecturer, 'Git', 10)
-student.rate_lectures(lecturer, 'Git', 10)
-student.rate_lectures(lecturer1, 'Python', 9)
+student.rate_lectures(lecturer, 'Python', 7)
+student.rate_lectures(lecturer, 'Python', 9)
+student.rate_lectures(lecturer, 'Python', 8)
+student.rate_lectures(lecturer1, 'Python', 5)
 student.rate_lectures(lecturer1, 'Python', 7)
 student.rate_lectures(lecturer1, 'Python', 9)
 
@@ -94,17 +104,18 @@ n_reviewer = Reviewer('Jonas', 'Bobby')
 n_reviewer.courses_attached += ['Python']
 some_reviewer = n_reviewer
 
-n_reviewer.rate_hw(student, 'Python', 9)
 n_reviewer.rate_hw(student, 'Python', 10)
 n_reviewer.rate_hw(student, 'Python', 10)
-n_reviewer.rate_hw(student1, 'Python', 9)
+n_reviewer.rate_hw(student, 'Python', 10)
+n_reviewer.rate_hw(student1, 'Python', 6)
 n_reviewer.rate_hw(student1, 'Python', 7)
-n_reviewer.rate_hw(student1, 'Python', 1)
+n_reviewer.rate_hw(student1, 'Python', 9)
 
 
-print('Рецензент:', some_reviewer,'\n')
+# print('Рецензент:', some_reviewer,'\n')
 print('Лектор:', some_lecturer, '\n')
 print('Студент:', some_student,'\n')
-
-
-
+# lecturer.__lt__(lecturer1)
+# print(average1 > average)
+print(student.grades)
+print(student1.grades)
